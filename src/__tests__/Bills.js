1ce1a4fe3e2e1583e100d4b15c54ce5426908be5
Kwaay <benoit.dum74@gmail.com>
2022-11-25 18:25:10 +0100

    ✨ NewBill tests and add comments

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -2,46 +2,126 @@
  * @jest-environment jsdom
  */
 
-import { screen, waitFor } from '@testing-library/dom';
+import { screen, waitFor, fireEvent, toHaveClass } from '@testing-library/dom';
+import userEvent from '@testing-library/user-event';
 import BillsUI from '../views/BillsUI.js';
 import { bills } from '../fixtures/bills.js';
-import { BillsContainer } from '../containers/Bills.js';
+import Bills from '../containers/Bills.js';
 import { ROUTES_PATH } from '../constants/routes.js';
 import { localStorageMock } from '../__mocks__/localStorage.js';
+import mockStore from '../__mocks__/store.js';
 
 import router from '../app/Router.js';
-
 describe('Given I am connected as an employee', () => {
   describe('When I am on Bills Page', () => {
+    /**
+     * ! Test for the bill icon in vertical layout to be highlighted
+     */
     test('Then bill icon in vertical layout should be highlighted', async () => {
+      // Je définis le localStorage avec les données du mock
       Object.defineProperty(window, 'localStorage', {
         value: localStorageMock,
       });
+      // Je définis le localStorage avec les données du mock
       window.localStorage.setItem(
         'user',
         JSON.stringify({
           type: 'Employee',
         }),
       );
+      // Je crée une div, où je lui set un id en tant que root et je le met dans le body
       const root = document.createElement('div');
       root.setAttribute('id', 'root');
       document.body.append(root);
+      // J'appelle le router
       router();
+      // Je navigue vers la page Bills
       window.onNavigate(ROUTES_PATH.Bills);
+      // J'attends que de récupèrer l'élement avec le data-testid = icon-window
       await waitFor(() => screen.getByTestId('icon-window'));
       const windowIcon = screen.getByTestId('icon-window');
-      //to-do write expect expression
+      // J'espère que le windowIcon est définie
+      expect(windowIcon).toBeDefined();
     });
+    /**
+     * ! Test for sorting the bills from earliest to latest
+     */
     test('Then bills should be ordered from earliest to latest', () => {
+      // J'insère le BillsUI dans le corps du document
       document.body.innerHTML = BillsUI({ data: bills });
+      // Je récupère les dates avec une regex
       const dates = screen
         .getAllByText(
           /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i,
         )
         .map((a) => a.innerHTML);
+      // J'indique que je les veux de manière inverse des aiguilles d'une montre (anti-horaire)
       const antiChrono = (a, b) => (a < b ? 1 : -1);
+      // Je les range de manière anti-horaire
       const datesSorted = [...dates].sort(antiChrono);
+      // J'espère que les dates seront rangés de manière anti-horaire
       expect(dates).toEqual(datesSorted);
+    });
+    /**
+     * ! Test for checking that the button create a new bill
+     */
+    test('Then new bill button should create a new bill', async () => {
+      // Je crée une nouvelle instance de Bills
+      const bills = new Bills({ document, onNavigate, localStorage });
+      // Je récupère le bouton "Nouvelle note de frais"
+      const newBillButton = screen.getByTestId('btn-new-bill');
+      // Je mock la fonction handleClickNewBill
+      const handleClickNewBill = jest.fn(bills.handleClickNewBill);
+      // Je met un addEventListener sur le bouton pour les clicks sur celui-ci
+      newBillButton.addEventListener('click', handleClickNewBill);
+      // Je simule un click sur le bouton
+      await userEvent.click(newBillButton);
+      // J'espère que la fonction a été appelée
+      expect(handleClickNewBill).toHaveBeenCalled();
+    });
+    /**
+     * ! Test for the new bill navigation
+     */
+    test('Then, I should be sent to newBill page', async () => {
+      // Je crée une nouvelle instace de Bills
+      const bills = new Bills({ document, onNavigate, localStorage });
+      // Je mock la fonction handleClickNewBill
+      const handleClickNewBill = jest.fn(bills.handleClickNewBill);
+      // Je récupère le bouton
+      const btnNewBill = screen.getByTestId('btn-new-bill');
+      // Je met un addEventListener pour les clicks sur le bouton et je lance la fonction mockée si un click est fait
+      btnNewBill.addEventListener('click', handleClickNewBill);
+      // Je simule le click sur le bouton
+      await userEvent.click(btnNewBill);
+      // J'espère que la fonction a été appelée
+      expect(handleClickNewBill).toHaveBeenCalled();
+    });
+    /**
+     * ! Test for clicking on the eye button
+     */
+    test('Then, I click on the eye button', async () => {
+      // Je mock le onNavigate
+      const onNavigate = jest.fn();
+      // Je crée une instance de Bills
+      const bills = new Bills({ document, onNavigate, localStorage });
+      // Je mock la function handleClickIconEye
+      const handleClickIconEye = jest.fn(bills.handleClickIconEye);
+      // Je récupère tous les boutons d'icon-eye
+      const btnIconEye = screen.getAllByTestId('icon-eye');
+      // Je récupère la modal
+      const modal = document.getElementById('modaleFile');
+      // Je mock l'ajout de la classe show sur la modal
+      $.fn.modal = jest.fn(() => modal.classList.add('show'));
+
+      // Pour chaque icon-eye..
+      btnIconEye.forEach(async (icon) => {
+        // Je set un addEventListener au click sur l'icône
+        icon.addEventListener('click', handleClickIconEye(icon));
+        // Je simule le click sur l'icône
+        await userEvent.click(icon);
+      });
+      // J'espère que la fonction a été appelée
+      expect(handleClickIconEye).toHaveBeenCalled();
     });
   });
 });
--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,17 +2,180 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
+import '@testing-library/jest-dom';
+import NewBillUI from '../views/NewBillUI.js';
+import NewBill from '../containers/NewBill.js';
+import { localStorageMock } from '../__mocks__/localStorage.js';
+import { fireEvent, screen } from '@testing-library/dom';
+import store from '../__mocks__/store.js';
 
+// Je mock le onNavigate
+const onNavigate = jest.fn();
+// Je mock un message d'erreur
+window.alert = jest.fn();
+// Je définis le localStorage avec les données du mock
+Object.defineProperty(window, 'localStorage', { value: localStorageMock });
+// Je set l'item 'user' en tant qu'employé
+window.localStorage.setItem(
+  'user',
+  JSON.stringify({
+    type: 'Employee',
+  }),
+);
 
-describe("Given I am connected as an employee", () => {
-  describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
-    })
-  })
-})
+describe('Given I am connected as an employee', () => {
+  describe('When I am on NewBill Page', () => {
+    /**
+     * ! Test for the page render
+     */
+    test('Then the page should be rendered correctly', () => {
+      // J'insère le NewBillUI dans le corps du document
+      document.body.innerHTML = NewBillUI();
+      // Je récupère le bouton "Envoyer une note de frais"
+      const newBillBtn = screen.getByText('Envoyer une note de frais');
+      // Je récupère le bouton "Envoyer"
+      const sendBtn = screen.getByText('Envoyer');
+      // Je récupère le formulaire
+      const form = document.querySelector('form');
+      // J'espère que le bouton "Envoyer une note de frais" sera défini
+      expect(newBillBtn).toBeDefined();
+      // J'espère que le bouton "Envoyer" sera défini
+      expect(sendBtn).toBeDefined();
+      // J'espère que le formulaire aura une longueur de 9
+      expect(form).toHaveLength(9);
+    });
+    /**
+     * ! Test for the upload on the image with a great extension
+     */
+    test('Then i want to upload a image file with a great extension', () => {
+      // J'insère le NewBillUI dans le corps du document
+      document.body.innerHTML = NewBillUI();
+      // Je crée une instance de NewBill
+      const newBillInstance = new NewBill({
+        document,
+        onNavigate,
+        store,
+        localStorage: window.localStorage,
+      });
+      // Je récupère le bouton d'envoi du fichier
+      const fileUploadBtn = screen.getByTestId('file');
+      // Je mock la fonction handleChangeFile
+      const handleChangeFile = jest.fn(newBillInstance.handleChangeFile);
+      // Je set un addEventListener sur le mock de la fonction handleChangeFile pour les changements
+      fileUploadBtn.addEventListener('change', handleChangeFile);
+      // Crée un évenement "change" avec un faux fichier de test
+      fireEvent.change(fileUploadBtn, {
+        target: {
+          files: [new File(['content'], 'imgtest.jpg', { type: 'image/jpg' })],
+        },
+      });
+      // Récupère le nombre de fichiers
+      const fileNumber = fileUploadBtn.files.length;
+      // J'espère que la fonction handleChangeFile sera appelée
+      expect(handleChangeFile).toHaveBeenCalled();
+      // J'espère que le nombre de fichiers soit supérieur à 0
+      expect(fileNumber).toBeGreaterThan(0);
+      // J'espère  que le nombre de fichiers soit inférieur à 2
+      expect(fileNumber).toBeLessThan(2);
+      // J'espère que le nom du fichier sera imgtest.jpg
+      expect(fileUploadBtn.files[0].name).toBe('imgtest.jpg');
+      // J'espère que la valeur du bouton d'upload de fichier ne soit pas null
+      expect(fileUploadBtn.value).not.toBeNull();
+    });
+    /**
+     * ! Test for the upload on the image with a bad extension
+     */
+    test('Then i want to upload a image file with a bad extension', () => {
+      // J'insère le NewBillUI dans le corps du document
+      document.body.innerHTML = NewBillUI();
+      // Je crée une instance de NewBill
+      const newBillInstance = new NewBill({
+        document,
+        onNavigate,
+        store,
+        localStorage: window.localStorage,
+      });
+      // Je récupère le bouton d'envoi du fichier
+      const fileUploadBtn = screen.getByTestId('file');
+      // Je mock la fonction handleChangeFile
+      const handleChangeFile = jest.fn(newBillInstance.handleChangeFile);
+      // Je set un addEventListener sur le mock de la fonction handleChangeFile pour les changements
+      fileUploadBtn.addEventListener('change', handleChangeFile);
+      // Crée un évenement "change" avec un faux fichier de test
+      fireEvent.change(fileUploadBtn, {
+        target: {
+          files: [new File(['content'], 'imgtest.gif', { type: 'image/gif' })],
+        },
+      });
+      // Récupère le nombre de fichiers
+      const fileNumber = fileUploadBtn.files.length;
+      // J'espère que la fonction handleChangeFile sera appelée
+      expect(handleChangeFile).toHaveBeenCalled();
+      // J'espère que le nombre de fichiers soit supérieur à 0
+      expect(fileNumber).toBeGreaterThan(0);
+      // J'espère  que le nombre de fichiers soit inférieur à 2
+      expect(fileNumber).toBeLessThan(2);
+      // J'espère que le nom du fichier sera imgtest.gif
+      expect(fileUploadBtn.files[0].name).toBe('imgtest.gif');
+    });
+  });
+  describe('When I submit a new valid bill', () => {
+    /**
+     * ! POST Test
+     */
+    test('Then a new bill should be created', () => {
+      // J'insère le NewBillUI dans le corps du document
+      document.body.innerHTML = NewBillUI();
+      // Je récupère le formulaire de création de Bills
+      const newBillForm = screen.getByTestId('form-new-bill');
+      // Je crée une instance de NewBill
+      const newBillInstance = new NewBill({
+        document,
+        onNavigate,
+        store,
+        localStorage: window.localStorage,
+      });
+      // Je mock la function handleSubmit
+      const handleSubmit = jest.fn(newBillInstance.handleSubmit);
+      newBillForm.addEventListener('submit', handleSubmit);
+      // Je définis une Bills valide pour une création
+      const newBillValid = {
+        name: 'BillTest',
+        type: 'Hôtel et logement',
+        date: '2022-11-25',
+        fileUrl: 'https://localhost:3456/images/imgtest.jpg',
+        fileName: 'imgtest.jpg',
+        amount: 123,
+        vat: 456,
+        pct: 789,
+      };
+      // Object destructuring for the newBillValid object
+      const {
+        type,
+        name,
+        date,
+        amount,
+        vat,
+        pct,
+        commentary,
+        fileUrl,
+        fileName,
+      } = newBillValid;
+      // Récupération de tous les champs et set de leur valeur avec les données de test
+      document.querySelector(`select[data-testid="expense-type"]`).value = type;
+      document.querySelector(`input[data-testid="expense-name"]`).value = name;
+      document.querySelector(`input[data-testid="datepicker"]`).value = date;
+      document.querySelector(`input[data-testid="amount"]`).value = amount;
+      document.querySelector(`input[data-testid="vat"]`).value = vat;
+      document.querySelector(`input[data-testid="pct"]`).value = pct;
+      document.querySelector(`textarea[data-testid="commentary"]`).value =
+        commentary;
+      newBillInstance.fileUrl = fileUrl;
+      newBillInstance.fileName = fileName;
+      // Créer un évenement de submit pour le form
+      fireEvent.submit(newBillForm);
+      // J'espère que la fonction handleSubmit sera appelé
+      expect(handleSubmit).toHaveBeenCalled();
+    });
+  });
+});
